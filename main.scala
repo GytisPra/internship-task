adcb8a0c23bc81c6bf0211e72defeec63bc0196a
GytisPra <gpgytis@gmail.com>
2025-07-02 21:02:37 +0300

    added function that checks if a location is inside a given region

--- main.scala
+++ main.scala
@@ -1,22 +1,61 @@
-//> using toolkit default
-
 import upickle.default.*
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import os.write.append
 
-case class Location(name: String, coordinates: List[Float]) derives ReadWriter
+case class Location(name: String, coordinates: (Double, Double)) derives Reader
 
-case class Region(name: String, coordinates: List[List[List[Float]]]) derives ReadWriter
+case class Region(name: String, @upickle.implicits.key("coordinates") polygons: Vector[Vector[(Double, Double)]]) derives Reader
+
+case class Result(region: String, var matchedLocations: Seq[String]) derives Writer:
+    def appendLocation(location: String): Unit =
+        this.matchedLocations = this.matchedLocations :+ location
 
 @main
-def main(): Unit = 
-    val locationsInputPath = os.pwd / "input/locations.json"
-    val regionsInputPath = os.pwd / "input/regions.json"
+def main(regions: String, locations: String, output: String): Unit = 
+    val locationsInputPath = os.pwd / "input" / locations.stripPrefix("locations=")
+    val regionsInputPath = os.pwd / "input" / regions.stripPrefix("regions=")
+    var resultsPath = os.pwd / "output" / output.stripPrefix("output=")
 
-    println(s"Attempting to read locations from directory ${locationsInputPath}")
-    println(s"Attempting to read regions from directory ${regionsInputPath}")
+    if os.exists(resultsPath) then
+        os.remove(resultsPath)
 
-    val locations: List[Location] = read[List[Location]](os.read(locationsInputPath))
-    val regions: List[Region] = read[List[Region]](os.read(regionsInputPath))
+    val readLocations: List[Location] = read[List[Location]](os.read(locationsInputPath))
+    val readRegions: List[Region] = read[List[Region]](os.read(regionsInputPath))
+    var results: List[Result] = List()
 
-    regions.map(r => println(r))
+    readRegions.map(reg => 
+        val result = new Result(reg.name, List())
+        readLocations.map(loc => 
+            if locationInPolygon(loc, reg) then
+                result.appendLocation(loc.name)
+                println(s"${reg.name}: ${loc.name}")
+        )
+        results = results :+ result  
+    )
 
-    
+    os.write(resultsPath, write(results))
+
+
+def locationInPolygon(location: Location, region: Region): Boolean =
+    val path: Path2D = new Path2D.Double
+    var matchedLocation = ""
+   
+    region.polygons.map { pol =>
+        path.moveTo(pol.head._1, pol.head._2)
+
+        pol.drop(1).map(coords => 
+            path.lineTo(coords._1, coords._2)
+        )
+
+        path.closePath()
+
+        val testPoint: Point2D = new Point2D.Double(location.coordinates._1, location.coordinates._2)
+
+        if path.contains(testPoint) then
+            println(s"${location.name} is inside ${region.name}")
+            matchedLocation = location.name
+    }
+
+    return matchedLocation != ""
---
+++ project.scala
@@ -0,0 +1,1 @@
+//> using toolkit 0.7.0
