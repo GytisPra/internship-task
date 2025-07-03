95bb28508768530c3c9212e08d4da80b16574f3b
GytisPra <gpgytis@gmail.com>
2025-07-04 00:25:17 +0300

    tried out doing a custom pickler for the region so that I could have List[Polygon] rather than List[List[(Double, Double)]]. used a for-yield to get the results

--- main.scala
+++ main.scala
@@ -1,63 +1,117 @@
 //> using toolkit 0.7.0
 
 import upickle.default.*
+import upickle.implicits.key
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
 import os.write.append
+import ujson.Arr
+import ujson.Obj
+import scala.collection.mutable.ArrayBuffer
+import scala.compiletime.ops.double
 
-case class Location(name: String, coordinates: (Double, Double)) derives Reader
+case class Location(val name: String, val coordinates: (Double, Double))
+    derives Reader
 
-case class Region(name: String, @upickle.implicits.key("coordinates") polygons: Vector[Vector[(Double, Double)]]) derives Reader
+case class Region(
+    val name: String,
+    val polygons: List[Polygon]
+)
 
-case class Result(region: String, var matchedLocations: Seq[String]) derives Writer:
-    def appendLocation(location: String): Unit =
-        this.matchedLocations = this.matchedLocations :+ location
+implicit val regionReader: Reader[Region] =
+  reader[ujson.Value].map[Region] {
+    case ujson.Obj(json) =>
+      val name     = json("name").str
+      val polygons = read[List[Polygon]](json("coordinates"))
+      new Region(name = name, polygons)
+    case other           =>
+      throw new Error(
+        s"error while trying to read regions exptected name and List(List(List())) got: $other"
+      )
+  }
+
+case class Polygon(val points: List[(Double, Double)])
+  // def formEdges(): List[(Double, Double, Double, Double)] =
+  //   points.map(point => point match {
+  //     case ((x1, y1), (x2, y2)) => (x1, y1, x2, y2)
+  //   })
+  //   }
+
+implicit val polygonsListReader: Reader[List[Polygon]] =
+  reader[ujson.Value].map[List[Polygon]] {
+    case ujson.Arr(polygonsArr) =>
+      polygonsArr.toList
+        .map {
+          case Arr(value) =>
+            value.toList.map {
+              case Arr(value) =>
+                value.toList match {
+                  case List(a, b) => (a.num, b.num)
+                  case other      =>
+                    throw new Error(s"Expected List(a, b) got: $other")
+                }
+              case other      => throw new Error(s"Expected Arr(value) got: $other")
+            }
+          case other      => throw new Error(s"Expected Arr(value) got: $other")
+        }
+        .map(Polygon(_))
+    case other                  =>
+      throw new Error(
+        s"error while trying to read polygons exptected Arr(value) got: $other"
+      )
+  }
+
+case class Result(val region: String, var matchedLocations: Seq[String])
+    derives Writer:
+  def appendLocation(
+      location: String
+  ): Unit = // Should not be doing this with a mutable list
+    this.matchedLocations = this.matchedLocations :+ location
 
 @main
-def main(regions: String, locations: String, output: String): Unit = 
-    val locationsInputPath = os.pwd / "input" / locations.stripPrefix("locations=")
-    val regionsInputPath = os.pwd / "input" / regions.stripPrefix("regions=")
-    var resultsPath = os.pwd / "output" / output.stripPrefix("output=")
+def main(regionsPath: String, locationsPath: String, outputPath: String): Unit =
+  val locInputPath =
+    os.pwd / "input" / locationsPath.stripPrefix("locations=")
+  val regInputPath =
+    os.pwd / "input" / regionsPath.stripPrefix("regions=")
+  var resPath      =
+    os.pwd / "output" / outputPath.stripPrefix("output=")
 
-    if os.exists(resultsPath) then
-        os.remove(resultsPath)
+  val regions: List[Region]     = read[List[Region]](os.read(regInputPath))
+  val locations: List[Location] = read[List[Location]](os.read(locInputPath))
+  var results: List[Result]     = List()
 
-    val readLocations: List[Location] = read[List[Location]](os.read(locationsInputPath))
-    val readRegions: List[Region] = read[List[Region]](os.read(regionsInputPath))
-    var results: List[Result] = List()
+  val result = for 
+    region <- regions  
+    location <- locations
+    if locationInPolygons(location, region.polygons)
+  yield (region.name, location.name)
+  // This for-yield outputs List((region1, location1), (region1, location2), ...)
+  // I need a way to group the results by the region name so that I get List((region1, List(...matchedLocations...)))
+  // and also for example region3 has no matched locations so I would like to have List(..., (region3, List()))
 
-    readRegions.map(reg => 
-        val result = new Result(reg.name, List())
-        readLocations.map(loc => 
-            if locationInPolygon(loc, reg) then
-                result.appendLocation(loc.name)
-                println(s"${reg.name}: ${loc.name}")
-        )
-        results = results :+ result  
-    )
+  val test = result.groupBy(result => result._1)
+  println(test)
+    
 
-    os.write(resultsPath, write(results))
+  if os.exists(resPath) then os.remove(resPath)
 
+  os.write.append(resPath, write[List[Result]](results))
 
-def locationInPolygon(location: Location, region: Region): Boolean =
-    val path: Path2D = new Path2D.Double
-    var matchedLocation = ""
-   
-    region.polygons.map { pol =>
-        path.moveTo(pol.head._1, pol.head._2)
+def locationInPolygons(location: Location, polygons: List[Polygon]): Boolean =
+  val path: Path2D    = new Path2D.Double
+  var locationMatched = false
 
-        pol.drop(1).map(coords => 
-            path.lineTo(coords._1, coords._2)
-        )
+  for polygon <- polygons do
+    path.moveTo(polygon.points.head._1, polygon.points.head._2)
 
-        path.closePath()
+    polygon.points.drop(1).map(point => path.lineTo(point._1, point._2))
 
-        val testPoint: Point2D = new Point2D.Double(location.coordinates._1, location.coordinates._2)
+    path.closePath()
 
-        if path.contains(testPoint) then
-            println(s"${location.name} is inside ${region.name}")
-            matchedLocation = location.name
-    }
+    val testPoint: Point2D =
+      new Point2D.Double(location.coordinates._1, location.coordinates._2)
 
-    return matchedLocation != ""
+    if path.contains(testPoint) then locationMatched = true
+
+  return locationMatched
