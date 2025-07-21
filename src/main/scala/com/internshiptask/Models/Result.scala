a3d053f812ba9c05102347adfc5fa9d48fbb79b2
GytisPra <gpgytis@gmail.com>
2025-07-21 18:29:50 +0300

    simplified results formating

--- src/main/scala/com/internshiptask/Models/Result.scala
+++ src/main/scala/com/internshiptask/Models/Result.scala
@@ -9,16 +9,7 @@ object Result {
       regions: List[Region],
       unformattedResults: List[(String, String)]
   ): List[Result] =
-    val groupedAndTransformedResults = unformattedResults
-      .groupBy(r => r._1) // Group by the region name
-      .transform((key, value) => // for the value we only need the location
-        value.map(valueTuple => valueTuple._2)
-      )
-      .map(resultTuple => Result(resultTuple._1, resultTuple._2))
-
-    regions.map(region =>
-      groupedAndTransformedResults.find(_.region == region.name) match
-        case None        => Result(region.name, List())
-        case Some(value) => value
-    )
+    val groupedResults = unformattedResults.groupMap(_._1)(_._2)
+    
+    regions.map(region => Result(region.name, groupedResults.getOrElse(region.name, List())))
 }
